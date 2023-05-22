78ffcf91f941e9d637d8ce585f3ac18d78d7b101
dboukh3 <47596552+dboukh3@users.noreply.github.com>
2023-05-22 13:29:06 +0300

    Add files via upload

---
+++ Project1/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Your code here
+    let serachForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    serachForm.classList.toggle('active');
+};
+
+});
+
+
